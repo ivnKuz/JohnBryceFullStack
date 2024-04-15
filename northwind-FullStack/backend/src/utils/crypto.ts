62880ae2b655a50910d05aeb36599271f491569d
CyberProger <booleansandwich@gmail.com>
2024-04-15 19:41:48 +0300

    angular northwind

--- northwind-FullStack/backend/src/utils/crypto.ts
+++ northwind-FullStack/backend/src/utils/crypto.ts
@@ -1,13 +1,17 @@
   
-import { createHash } from 'crypto';
+import { createHash, createHmac } from 'crypto';
 import userDTO from '../models/auth/user-dto'
 import { sign } from 'jsonwebtoken';
   //encrypt password, md5 has a problem that it saves the same strings in same encryption
   export function hashPassword(plainTextPassword:string, salt:string): string{
-    return createHash('md5')
-    .update(`${plainTextPassword}${salt}`) // <= adding some text to hashed password "SALTING"
+    // return createHash('md5')
+    // .update(`${plainTextPassword}${salt}`) // <= adding some text to hashed password "SALTING"
+    // .digest('hex');
+    //better salting, spreading it in password
+ return createHmac('md5', salt)
+    .update(`${plainTextPassword}`)
     .digest('hex');
-}
+  }
 
 //returns json web token jwt
 export function generateJWT(user: userDTO, secret: string, expiresIn: string): string {
